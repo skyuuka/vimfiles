e265ada5ec5150d4ee349d587a4d16bf941c25d7
Chen <linche@amazon.com>
2014-12-04 08:26:49 -0800

    add comments

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# install neobundle
+# install neobundle: works on Unix and OSX
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
 cp -i vimrc ~/.vimrc
 cp -i utils.vim ~/.vim/
